9fc05b817705c667ae8cdc2d2fab29c1e3060ea0
Shamna <shamnahajara@gmail.com>
2025-02-10 16:38:26 +0530

    Portfolio ui added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import logo from "../assets/shamna-logo.webp"
+import {  FaGithub, FaInstagram, FaLinkedin, FaXTwitter } from 'react-icons/fa6'
+
+function Navbar() {
+  return (
+    <nav className='flex items-center justify-between py-6'>
+        <div className='flex flex-shrink-0 items-center '> 
+            <a href='/' aria-label='Home' className=''>
+                <img src={logo} alt='DevDojo Logo' className='max-2' width={50} height={33}/>
+            </a>
+        </div>
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+            <a href='https://www.linkedin.com/in/hajarathu-shamna-755122273/' target='_blank' rel='nopener noreferrer' aria-label='LinkedIn'>
+                <FaLinkedin/>
+            </a>
+            <a href='https://github.com/Shamnahajara' target='_blank' rel='nopener noreferrer' aria-label='GitHub'>
+                <FaGithub/>
+            </a>
+            <a href='https://www.instagram.com/sham_na002?igsh=NWN6aWozY2cwZTI3' target='_blank' rel='nopener noreferrer' aria-label='Instagram'>
+                <FaInstagram/>
+            </a>
+            <a href='https://twitter.com' target='_blank' rel='nopener noreferrer' aria-label='Twitter'>
+                <FaXTwitter/>
+            </a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,55 @@
+import project1 from "../assets/projects/Freshcrate.png";
+import project2 from "../assets/projects/i4businesscard.png";
+import project3 from "../assets/projects/TeamkarmaCRM.png";
+import project4 from "../assets/projects/ZodicPulse.png";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1+ year of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 Dec - Present",
+    role: "Junior Full Stack Developer",
+    company: "I4interface",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript","TypeScript" ,"React.js", "Next.js", "mongoDB","Svelte"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Business Card Generator",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "CRM System",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Astrology website",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Malappuram,Kerala,India",
+  phoneNo: "+91 8129383575 ",
+  email: "shamnahajara@gmail.com",
+};
